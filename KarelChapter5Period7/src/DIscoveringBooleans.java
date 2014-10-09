a79857f497e4e49fd1c09dac4b7d3120b7b9414f
steventhanna <steventhanna@gmail.com>
2014-10-09 13:39:57 -0500

    Conditionals today

--- KarelChapter5Period7/src/DIscoveringBooleans.java
+++ KarelChapter5Period7/src/DIscoveringBooleans.java
@@ -3,7 +3,21 @@ import kareltherobot.*;
 public class DIscoveringBooleans {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x = 0;
+		boolean isWet = true;
+		boolean isDry = false;
+		
+		if(isWet == true) {
+			System.out.println("It is wet");
+		} else {
+			System.out.println("It is dry");
+		}
+ 		
+		if(x >= 0) {
+			System.out.println("It is wet");
+		} else if (x < 0){
+			System.out.println("X is a non-negative");
+		}
 
 	}
 
