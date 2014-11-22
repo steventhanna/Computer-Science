eca7b2116f1139e10cba04a89ccc155d47ed170c
steventhanna <steventhanna@gmail.com>
2014-11-21 22:18:23 -0600

    Added program2

---
+++ Chapter 2/VariablesAndStuff/src/Program2.java
@@ -0,0 +1,13 @@
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value1 = 2147483647 + 1;
+		System.out.println(value1);
+		int value2 = -2147483647 - 1;
+		System.out.println(value2);
+
+	}
+
+}
