2a708d585aec55741025c7795b1de01e338aa84c
steventhanna <steventhanna@gmail.com>
2014-11-24 13:02:23 -0600

    Added more code

--- Chapter 2/VariablesAndStuff/src/ComputeTime.java
+++ Chapter 2/VariablesAndStuff/src/ComputeTime.java
@@ -6,14 +6,45 @@ public class ComputeTime {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Seconds: ");
-		int seconds = scanner.nextInt();
+		double seconds = scanner.nextDouble();
 		scanner.close();
 		
 		// Ask a user for a time in seconds and change it to minutes
-		int minutes = seconds / 60;
-		int numberOfSeconds = seconds % 60;
+		double minutes = seconds / 60;
+		double numberOfSeconds = seconds % 60;
 		
-		System.out.println("The amount of time = " + minutes + " minutes " + numberOfSeconds + " seconds");
+		
+		
+		// Number of hours
+		
+		double hours = minutes / 60;
+		double numberOfMinutes = minutes % 60;
+		if(numberOfMinutes < 0) {
+			numberOfMinutes = 0;
+		}
+		double days = hours / 24;
+		double numberOfHours = hours % 24;
+		if(numberOfHours < 0) {
+			numberOfHours = 0;
+		}
+		
+		double weeks = days / 7;
+		double numberOfDays = days % 7;
+		if(numberOfDays < 0) {
+			numberOfDays = 0;
+		}
+		
+		double years = weeks / 52;
+		double numberOfWeeks = weeks % 52;
+		if(numberOfWeeks < 0) {
+			numberOfWeeks = 0;
+		}
+		
+		if(years < 0) {
+			years = 0;
+		}
+ 		
+		System.out.println("The amount of time = " + years + " years " + numberOfWeeks + " weeks "+ numberOfDays + " days " + numberOfHours + " hours " + numberOfMinutes + " minutes " + numberOfSeconds + " seconds");
 		
 		
 	}
