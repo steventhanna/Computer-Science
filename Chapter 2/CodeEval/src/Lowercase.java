ca418abf42b120cf02847da257e62f005e691ccb
steventhanna <steventhanna@gmail.com>
2014-12-05 13:04:57 -0600

    Notes 6

---
+++ Chapter 2/CodeEval/src/Lowercase.java
@@ -0,0 +1,11 @@
+
+public class Lowercase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String text = "HELLO CODEEVAL";
+		String moreText = "This is some text";
+		text = text.toLowerCase();
+		moreText = moreText.toLowerCase();	
+	}
+}
---
+++ Chapter 2/VariablesAndStuff/src/PlayingWithStrings.java
@@ -0,0 +1,11 @@
+
+public class PlayingWithStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String word = "Eddie";
+		String anotherWord = "is";
+		String lastWord = "Ethnic";
+	}
+
+}
