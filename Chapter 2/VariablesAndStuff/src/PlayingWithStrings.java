ff74f777887872c88411d5d7409e1c98d608d965
steventhanna <steventhanna@gmail.com>
2014-12-05 13:39:52 -0600

    Added switch program

--- Chapter 2/VariablesAndStuff/src/PlayingWithStrings.java
+++ Chapter 2/VariablesAndStuff/src/PlayingWithStrings.java
@@ -6,6 +6,33 @@ public class PlayingWithStrings {
 		String word = "Eddie";
 		String anotherWord = "is";
 		String lastWord = "Ethnic";
+		
+		
+		// String concatenation
+		String combo = word + " " + anotherWord + " " + lastWord;
+		String concatCombo = word.concat(anotherWord.concat(lastWord));
+		
+		System.out.println(word);
+		System.out.println(combo);
+		
+		System.out.println("The sentence above has " + combo.length() + " characters");
+		
+		System.out.println(concatCombo);
+		
+		System.out.println("The sentence above has " + concatCombo.length() + " characters");
+		
+		// subString() method
+		String differentCombo = combo.substring(0, 9) + "Fun";
+		System.out.println(differentCombo);
+		
+		// toLowerCase() and toUpperCase()
+		System.out.println(differentCombo.toUpperCase());
+		System.out.println(differentCombo.toLowerCase());
+		
+		System.out.println(differentCombo + 7);
+		
+		System.out.println("hello \n my name \t is \t steven");
+		
 	}
 
 }
