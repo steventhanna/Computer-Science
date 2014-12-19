49b54f99dc0c1a21714c9bf505b5c5bdcf9ce216
steventhanna <steventhanna@gmail.com>
2014-12-18 22:32:01 -0600

    Completed question4

--- Chapter 2/SHanna_Chapter_2_Programs/src/Question4.java
+++ Chapter 2/SHanna_Chapter_2_Programs/src/Question4.java
@@ -7,22 +7,31 @@
  * simplicity, assume a year has 365 days.
  */
 
-import java.util.*;
-
 public class Question4 extends HelpfulMethods {
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int oneDay = 1440; // minutes
+		int oneYear = 525600; // number of minutes in a year, 365 days
+		
 		double minutes = inputDouble("Enter the amount of minutes");
-		// Amount of mins in a day = 1440
-		double years = minutes / 525949;
-		double days = minutes % 525949;
-		double remainingDays = days / 1440;
-		double remainingMinutes = remainingDays / 1440;
-		print("Amount of years " + years);
-		print("Amount of days " + remainingDays);
-		print("Amount of minutes " + remainingMinutes);
+		
+		double convertToYears = minutes / oneYear; // calculate the amount of years
+		double convertToDays = ((minutes % oneYear) / oneDay);
+		double convertToMinutes = ((minutes % oneDay));
+		convertToYears = Math.round(convertToYears - .5);
+		convertToDays = Math.round(convertToDays);
+		convertToMinutes = Math.round(convertToMinutes);
+		
+//		double years = minutes / 525949;
+//		years = Math.round(years + .5);
+		// Years works correctly 
+		
+		print(convertToYears + " years");
+		print(convertToDays + " days");
+		print(convertToMinutes + " minutes");
+		
 		
 
 	}
