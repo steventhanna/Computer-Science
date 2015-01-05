33ec4ac3dbc0c041bbaf51f3ec50eed4e4fbc692
steventhanna <steventhanna@gmail.com>
2015-01-05 15:28:23 -0600

    Added mprogram 1

--- Chapter 4/Chapter 4 Program Excersises/src/RepeatAdditionQuiz.java
+++ Chapter 4/Chapter 4 Program Excersises/src/RepeatAdditionQuiz.java
@@ -11,26 +11,67 @@ import java.math.*;
 
 public class RepeatAdditionQuiz {
 
-	private int[] randomNumbers = new int[3];
+	private static int[] randomNumbers = new int[3];
+	private static int userSum;
 	
-	public void generateRandomNumbers() {
+	public static void generateRandomNumbers() {
 		Random randy = new Random();
 		randomNumbers[0] = randy.nextInt(10);
 		randomNumbers[1] = randy.nextInt(10);
 		randomNumbers[2] = randomNumbers[0] + randomNumbers[1];
 	}
 	
-	public void getInput() {
+	public static void getInput() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println(randomNumbers[0]);
 		System.out.println(randomNumbers[1]);
 		System.out.println("Add the above numbers");
-		
+		userSum = scanner.nextInt();
+	}
+
+	public static boolean checkSums() {
+		if(userSum == randomNumbers[2]) {
+			System.out.println("Correct!");
+			return true;
+		} else {
+			System.out.println("Incorrect.");
+			return false;
+		}
+	}
+
+	// Call this method when inital input fails
+	public static void getSecondaryInput() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Try to add the numbers again");
+		userSum = scanner.nextInt();
+	}
+
+	public static boolean differentCheck() {
+		while(userSum != randomNumbers[2]) {
+			getSecondaryInput();
+			differentCheck();
+			return false;
+		}
+		System.out.println("Correct");
+		return true;
+	}
+
+	public static void go() {
+		// Randomize numbers
+		generateRandomNumbers();
+		// Ask user to calculate sum
+		getInput();
+		if(!checkSums()) {
+			getSecondaryInput();
+			differentCheck();
+		} else {
+			System.out.println("Correct");
+		}
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		go();
 	}
 
 }
