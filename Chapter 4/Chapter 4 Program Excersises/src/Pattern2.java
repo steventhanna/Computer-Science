ad33c279a3b72e9f418df165e3fe027109fe1b0d
steventhanna <steventhanna@gmail.com>
2015-01-13 13:38:02 -0600

    Pattern2

---
+++ Chapter 4/Chapter 4 Program Excersises/src/Pattern2.java
@@ -0,0 +1,18 @@
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		String s = "*";
+		for(int i=0; i<=7; i++) {
+			System.out.println();
+			for(int k=6; k>i; k--) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<=i; j++) {
+				System.out.printf("%1s", s);
+			}
+		}
+
+	}
+
+}
