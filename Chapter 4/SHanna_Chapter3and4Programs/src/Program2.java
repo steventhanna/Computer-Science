302325de51f113e869e3a3bc437865f6bf1137dc
steventhanna <steventhanna@gmail.com>
2015-01-17 15:23:51 -0600

    Finihsed program 2

--- Chapter 4/SHanna_Chapter3and4Programs/src/Program2.java
+++ Chapter 4/SHanna_Chapter3and4Programs/src/Program2.java
@@ -7,11 +7,18 @@ public class Program2 {
 	private static int scissor = 2;
 	private static int userNumber;
 	private static int AI;
+	private static int counter = 0;
+	private static int userWins = 0;
+	private static int AIWins = 0;
 	
 	public static void getUserInput() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter your move: rock (0), paper (1), scissor (2) ");
 		userNumber = scanner.nextInt();
+		if((userNumber > 2) || (userNumber < 0)) {
+			System.out.println("The number you entered was not 0, 1, or 2.  Please try again.");
+			getUserInput();
+		}
 	}
 	
 	public static void generateAINumber() {
@@ -22,14 +29,37 @@ public class Program2 {
 	public static void compareAnswers() {
 		if(userWin(userNumber, AI)) {
 			System.out.println("User wins");
+			userWins++;
 		} else {
-			System.out.println("User loses");
+			System.out.println("AI wins");
+			AIWins++;
 		}
-		System.out.println("AI has: " + AI);
+		if(AI == 0) {
+			System.out.println("AI is rock");
+		} else if(AI == 1) {
+			System.out.println("AI is paper");
+		} else {
+			System.out.println("AI is scissor");
+		}
+		if(userNumber == 0) {
+			System.out.println("User is rock");
+		} else if(userNumber == 1) {
+			System.out.println("User is paper");
+		} else {
+			System.out.println("User is scissor");
+		}
+		System.out.println("Total user wins: " + userWins);
+		System.out.println("Total AI wins: " + AIWins);
+		
+		
+		
 	}
 	
 	public static boolean userWin(int user, int AI) {
-		if((user == 0) && (AI == 2)) {
+		if(user == AI) {
+			System.out.println("Tie");
+			return false;
+		} else if((user == 0) && (AI == 2)) {
 			return false;
 		} else if((AI == 0) && (user == 2)) {
 			return true;
@@ -41,9 +71,19 @@ public class Program2 {
 	}
 	 
 	public static void go() {
-		getUserInput();
-		generateAINumber();
-		compareAnswers();
+		while((userWins < 2) && AIWins < 2) {
+			getUserInput();
+			generateAINumber();
+			compareAnswers();
+			counter++;
+		}
+		if(userWins == 2) {
+			System.out.println("Game terminated.  User has won twice.");
+		} else {
+			System.out.println("Game terminated.  AI has won twice.");
+		}
+
+
 	}
 	
 	public static void main(String[] args) {
