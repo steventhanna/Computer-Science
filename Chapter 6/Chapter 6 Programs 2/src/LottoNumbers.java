6b384fcd3ae980e7cde512457b88fc4780f2fd4b
steventhanna <steventhanna@gmail.com>
2015-02-18 21:15:00 -0600

    Added first homework assignment LottoNumbers

--- Chapter 6/Chapter 6 Programs 2/src/LottoNumbers.java
+++ Chapter 6/Chapter 6 Programs 2/src/LottoNumbers.java
@@ -1,72 +1,58 @@
-import java.io.*;
-import java.util.*;
+/**
+* @author Steven Hanna
+* @class LottoNumber
+* @description read ticket numbers from command line and determines
+* 1 - 99 are used
+* @date 2/18/15
+*/
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class LottoNumbers {
 
-	private String filename;
-	private ArrayList<String> numbersString = new ArrayList<String>();
-	private int[] numbers;
-	private boolean[] listToCheck = new boolean[100];
-	
-	public LottoNumbers(String file) {
-		filename = file;
-	}
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		boolean[] isCovered = new boolean[100]; // default == fault
 
-	public void readLottoTicket() throws IOException {
-		BufferedReader in = new BufferedReader(new FileReader(filename));
-		while(in.ready()) {
-			String num = in.readLine();
-			String[] nums = num.split("\\s");
-			for(int i = 0; i < nums.length; i++) {
-				numbersString.add(nums[i]);
-			}
+		// Read all numbers and mark corresponding id true
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		int response = 1;
+		while(response != 0) {
+			int num = scanner.nextInt();
+			numbers.add(num);
+			response = num;
 		}
-		in.close();
-	}
+		int[] numberArray = new int[numbers.size()];
+		for(int i = 0; i < numberArray.length; i++) {
+			numberArray[i] = numbers.get(i);
+		}
+		
+		for(int i = 0; i < numberArray.length; i++) {
+			isCovered[numberArray[i]] = true;
+		}
 
-	public void stringToInt() {
-		numbers = new int[numbersString.size()];
-		for(int i = 0; i < numbers.length; i++) {
-			String numToChange = numbersString.get(i);
-			numbers[i] = Integer.parseInt(numToChange);
-		}
-	}
-
-	public boolean checkValidity() throws IOException {
-		readLottoTicket();
-		stringToInt();
-		for(int i = 0; i < numbers.length; i++) {
-			listToCheck[numbers[i]] = true;
-		}
-		boolean valid;
-		for(int i = 0; i < listToCheck.length; i++) {
-			if(listToCheck[i] = true) {
-				valid = true;
-			} else {
-				valid = false;
+		// Check if all covered
+		boolean allCovered = true; // Assume all true
+		for(int i = 0; i < 99; i++) {
+			// Find one number not covered
+			// Use break statement to exit loop
+			if(isCovered[i] == false) {
+				allCovered = false;
 				break;
 			}
 		}
-		if(valid = true) {
-			return true;
-		} else {
-			return false;
+		for(int i = 0; i < isCovered.length; i++){
+			System.out.print(i + " ");
+			System.out.println(isCovered[i]);
 		}
-	}
 
-	public void go() throws IOException {
-		if(checkValidity() == false) {
-			System.out.println("The ticket covers all numbers");
+		// Display result
+		if(allCovered == true) {
+			System.out.println("The tickets cover all numbers");
 		} else {
 			System.out.println("The tickets don't cover all the numbers");
 		}
-	}
-
-	public static void main(String[] args) throws IOException {
-		LottoNumbers ticket = new LottoNumbers("lotto.txt");
-		LottoNumbers ticket2 = new LottoNumbers("lotto2.txt");
-		ticket.go();
-		ticket2.go();
 	}
 	
 }
