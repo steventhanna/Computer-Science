03f4fee837ed13b625ac8232380b4f51528a08fb
steventhanna <steventhanna@gmail.com>
2015-04-07 13:33:33 -0500

    Eddies stupid shit

--- Chapter 9/Chapter 9/src/WorkingWithStrings.java
+++ Chapter 9/Chapter 9/src/WorkingWithStrings.java
@@ -5,8 +5,28 @@ public class WorkingWithStrings {
 		// All strings are immutable objects
 		// Objects cannot be changed
 		
+		// Literal strings
 		String s1 = "Hello world";
 		s1 = "Hello world again";
+
+		// String constructor
+		String s2 = new String("Constructer String");
+		String s3 = "Literal String";
+		// System.out.println(s4 == s3);
+
+
+		String myString = "Eddie is ethnic and he hates that word";
+		int len = myString.length();
+		char ch = myString.charAt(len - 4);
+		System.out.println(ch);
+
+		// substrings
+		// Methods that create new Strings
+		String s10 = myString.substring(4);
+		System.out.println(s10);
+
+		String otherString = " not ";
+		System.out.println(myString.substring(0, 9) + otherString + myString.substring(9));
 
 	}
 
