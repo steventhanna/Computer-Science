f11c8cc550ab2c080c6147c0b434ba0732ca2693
steventhanna <steventhanna@gmail.com>
2015-04-07 12:59:55 -0500

    added working with strings

---
+++ Chapter 9/Chapter 9/src/WorkingWithStrings.java
@@ -0,0 +1,13 @@
+
+public class WorkingWithStrings {
+
+	public static void main(String[] args) {
+		// All strings are immutable objects
+		// Objects cannot be changed
+		
+		String s1 = "Hello world";
+		s1 = "Hello world again";
+
+	}
+
+}
