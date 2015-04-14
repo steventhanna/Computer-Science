ad4616f41c756d4b2bc8a734fba99b091116b17c
steventhanna <steventhanna@gmail.com>
2015-04-13 22:45:24 -0500

    Updated wrapper class

--- Chapter 9/Chapter 9/src/WrapperClasses.java
+++ Chapter 9/Chapter 9/src/WrapperClasses.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 
 public class WrapperClasses {
 	
@@ -7,10 +9,11 @@ public class WrapperClasses {
 		Double obj2 = new Double(15.5);
 		Character ob3 = new Character('c');
 
-		Integer obj1 = new Integer(70);
+		Integer obj1 = new Integer(10);
 		Integer obj4 = new Integer(30);
 
-		System.out.println(obj1.compareTo(obj4));
+		System.out.println(Integer.valueOf("EDDIE", 16));
+//		System.out.println(obj1.compareTo(obj4));
 	}
 
 }
