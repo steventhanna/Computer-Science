02f7f815615c62c6616104d933af321a134d4ef4
steventhanna <steventhanna@gmail.com>
2015-04-20 12:31:09 -0500

    Updated Eddies shitty code

--- Chapter 10/Inheritance/src/RightTriangle.java
+++ Chapter 10/Inheritance/src/RightTriangle.java
@@ -1,10 +1,9 @@
 
-public class RightTriangle extends Triangle
-{
+public class RightTriangle extends Triangle {
+	
 	private String type = "Right Trianlge"; 
 	
-	public RightTriangle(int side1, int side2, int side3)
-	{
+	public RightTriangle(int side1, int side2, int side3) {
 		super(side1, side2, side3); 
 		// If you are to call the super constructor
 		// it MUST MUST be the first line of code.  
@@ -13,19 +12,15 @@ public class RightTriangle extends Triangle
 	}
 	
 	
-	public boolean isRightTriangle(int s1, int s2, int s3)
-	{
+	public boolean isRightTriangle(int s1, int s2, int s3) {
 		int large = Math.max(s1, Math.max(s2, s3)); 
-		
-		if(s1 == large)
+		if(s1 == large) {
 			return Math.pow(s2, 2) + Math.pow(s3, 2) == Math.pow(large, 2); 
-			
-		else if(s2 == large)
+		} else if(s2 == large) {
 			return Math.pow(s1, 2) + Math.pow(s3, 2) == Math.pow(large, 2); 
-			
-		else 
+		} else { 
 			return Math.pow(s1, 2) + Math.pow(s2, 2) == Math.pow(large, 2); 
-		
+		}
 		
 	}
 }
--- Chapter 10/Inheritance/src/Triangle.java
+++ Chapter 10/Inheritance/src/Triangle.java
@@ -1,6 +1,5 @@
 
-public class Triangle 
-{
+public class Triangle {
 	// instance variables
 	private String type = "Generic Triangle"; 
 	private int side1; 
@@ -14,50 +13,42 @@ public class Triangle
 		side3 = 5; 
 	}*/
 	
-	public Triangle(Triangle tri)
-	{
+	public Triangle(Triangle tri) {
 		this(tri.side1, tri.side2, tri.side3); 
 	}
 	
-	public Triangle(int side1, int side2, int side3)
-	{
-		if(Triangle.isTriangle(side1, side2, side3))
-		{	
+	public Triangle(int side1, int side2, int side3) {
+		if(Triangle.isTriangle(side1, side2, side3)) {	
 				this.side1 = side1;
 				this.side2 = side2; 
 				this.side3 = side3;
+		} else {
+			throw (new IllegalArgumentException("Invalid Side Lengths"));
 		}
-		else throw (new IllegalArgumentException("Invalid Side Lengths"));
 			 
 	}
 	
-	public void setSide1(int side)
-	{
+	public void setSide1(int side) {
 		side1 = side; // You want test if this is valid.
 	}
 	
-	public void setSide2(int side)
-	{
+	public void setSide2(int side) {
 		side2 = side; // You want test if this is valid.
 	}
 	
-	public void setSide3(int side)
-	{
+	public void setSide3(int side) {
 		side3 = side; // You want test if this is valid.
 	}
 	
-	public int getPerimeter()
-	{
+	public int getPerimeter() {
 		return side1 + side2 + side3; 
 	}
 	
-	public double getArea()
-	{
+	public double getArea() {
 		double semiP = getPerimeter() / 2.0; 
 		
 		return Math.sqrt(semiP * (semiP - side1) * (semiP - side2) 
 				* (semiP - side3)); 
-		
 	}
 	
 	
